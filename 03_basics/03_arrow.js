b84f19f78033fc04dde02446babf95f333ad8f20
Gaurav Sharma <gaurav051sharma@gmail.com>
2026-04-17 20:20:57 +0000

    arrow

--- 03_basics/03_arrow.js
+++ 03_basics/03_arrow.js
@@ -27,7 +27,7 @@ Story of "this" keyword till now:
 Writing "this" in global scope gives output {} and this.username gives
 undefined.
 Writing "this" in function gives a long information (non-strict mode).
-Writing this.username in function gives undefined.
+Writing this.username in function gives undefined (strict mode).
 Writing "this" in a function inside an object gives the whole information
 about the object. Writing this.username in a function inside an object
 gives the value of the variable username.
@@ -37,9 +37,41 @@ Best use of "this" keyword---> Inside an object
 
 const result=function(){
     let username= "Gaurav"
-    console.log(this)             // Gives long information
+    // console.log(this)             // Gives long information
     // console.log(this.username)    // Gives undefined
 }
 
 result()
 
+// Arrow function
+
+const value= () => {
+    let username="Gaurav"
+    // console.log(this)                // empty {}
+    // console.log(this.username)       // undefined
+}
+
+value()
+
+const addNos= (num1,num2) => {
+    return num1+num2
+}
+
+// console.log(addNos(3,4))
+
+// Implicit Return ---> In functions curly braces {} are required if you
+// use return statement. Implicit return means to execute functions of
+// single line without {} and return statement and the value is returned
+// by using if required parentheses (). [Only for arrow functions]
+
+const add1= (num1,num2) => num1 +  num2
+const add2= (num1,num2) => (num1 +  num2)
+
+// Returning objects using implicit return
+// Without () for objects output will be undefined in implicit return
+
+const object= () => ({username:"Gaurav"})
+
+// console.log(add1(5,6))
+// console.log(add2(8,5))
+// console.log(object())
