2ba38f4e6768ed745f7ed54e8773117069afbb4c
Gaurav Sharma <gaurav051sharma@gmail.com>
2026-04-21 23:41:21 +0000

    iterations

---
+++ 05_iterations/01_for_loop.js
@@ -0,0 +1,56 @@
+// For Loop
+
+for (let i = 0; i < 10; i++) {
+    const element = i
+    // console.log(element)
+}
+
+// console.log(element)   // Error ---> out of block scope
+
+for (let i = 0; i < 10; i++) {
+    const element = i
+    if(element === 5){
+        // console.log("5 is the best number")
+    }
+    // console.log(element)
+}
+
+for(let i = 1; i <= 5; i++){
+    // console.log(`Outer loop is ${i}`)
+    for(let j=1; j<=5; j++){
+        // console.log(`Value of inner loop is ${j}`)
+    }
+}
+
+for(let i=1; i<=10; i++){
+    // console.log(`Outer loop value: ${i}`)
+    for(let j=1; j<=10; j++){
+        // console.log(`${i} * ${j} = ${i*j}`)
+    }
+}
+
+let myArray=["Flash","Batman","Superman"]
+// console.log(myArray.length)
+
+for(let i=0;i<myArray.length;i++){
+    const element=myArray[i]
+    // console.log(element)
+}
+
+// break and continue
+
+for(let i=1; i<=20; i++){
+    if(i == 5){
+        // console.log("Detected 5")
+        break
+    }
+    // console.log(`Value of i is ${i}`)
+}
+
+for(let i=1; i<=20; i++){
+    if(i == 5){
+        console.log("Detected 5")
+        continue
+    }
+    console.log(`Value of i is ${i}`)
+}
---
+++ 05_iterations/two.js
@@ -0,0 +1,29 @@
+// While Loop
+
+let i=1
+while(i <= 10){
+    // console.log(`Value of i is ${i}`)
+    i=i+2    // i+=2
+}
+
+let myArray=["Flash","Batman","Superman"]
+
+let j=0
+while(j < myArray.length){
+    // console.log(`Value is ${myArray[j]}`)
+    j=j+1    // j+=1   j++
+}
+
+let k=1
+do {
+    // console.log(`Value of k is ${k}`)
+    k++
+} while (k<=10)
+
+let m=11
+do {
+    console.log(`Value of m is ${m}`)
+    m++
+} while (m<=10)
+
+// do-while loop is executed atLeast once even if the condition is false
